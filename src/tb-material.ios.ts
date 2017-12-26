ccf306d396b0ebfa1a64ef2f63dfa76f08d7d7d1
Dean Grande <dean@DeanMac.gateway>
2017-12-26 23:33:46 +1100

    green md button showing (demo not demo-ng)

--- src/tb-material.ios.ts
+++ src/tb-material.ios.ts
@@ -6,7 +6,25 @@ export class TbMaterial extends Common {
         console.log(MDCRaisedButton);
     }
     createNativeView() {
-        console.log('create native view');
-        return this.nativeView;
+        console.log('create native view 3');
+        const btn = MDCRaisedButton.new();
+
+        let colorModule = require("color");
+        let red = new colorModule.Color("#ff0000");
+
+        // let raiseButton = btn.init();
+
+        // raiseButton.titleLabel = "TEST";
+        // raiseButton.setTitle("Raised Button", forState: .Normal);
+        // btn.sizeToFit();
+        // raiseButton.addTarget(self, action: "tapped:", forControlEvents: .TouchUpInside);
+        // this.nativeView.addSubview(btn);
+
+        // self.view.addSubview(raiseButton);
+        // btn.backgroundColor = red;
+        btn.setTitleForState("ola", UIControlState.Normal);
+
+        btn.setBackgroundColor(MDCPalette.greenPalette.tint400);
+        return btn;
     }
 }
